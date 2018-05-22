5d5ece63d45f1ec9a529250eacfa5a28f621d778
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-22 16:45:33 -0400

    Add the rest of auth0's logic to auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 /* global auth0 env */
-// Copied from auth0's quickstart single-page login guide.
+// Copied from auth0"s quickstart single-page login guide.
 window.addEventListener("load", function() {
 
   // Establish a connection to auth0 and get a token.
@@ -23,6 +23,80 @@ window.addEventListener("load", function() {
     webAuth.authorize();
   });
 
-  
+  // Capture some more DOM elements.
+  var loginStatus = document.querySelector(".container h4");
+  var loginView = document.getElementById("login-view");
+  var homeView = document.getElementById("home-view");
 
-});
+  // buttons and event listeners
+  var homeViewBtn = document.getElementById("btn-home-view");
+  var logoutBtn = document.getElementById("btn-logout");
+
+  homeViewBtn.addEventListener("click", function() {
+    homeView.style.display = "inline-block";
+    loginView.style.display = "none";
+  });
+
+  logoutBtn.addEventListener("click", logout);
+
+  function handleAuthentication() {
+    webAuth.parseHash(function(err, authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = "";
+        setSession(authResult);
+        loginBtn.style.display = "none";
+        homeView.style.display = "inline-block";
+      } else if (err) {
+        homeView.style.display = "inline-block";
+        console.log(err);
+        alert(
+          "Error: " + err.error + ". Check the console for further details."
+        );
+      }
+      displayButtons();
+    });
+  }
+
+  function setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    var expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+  }
+
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    displayButtons();
+  }
+
+  function isAuthenticated() {
+    // Check whether the current time is past the
+    // Access Token"s expiry time
+    var expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+    return new Date().getTime() < expiresAt;
+  }
+
+  // Auth0 does this in a very funny way that I will gladly refactor.
+  function displayButtons() {
+    if (isAuthenticated()) {
+      loginBtn.style.display = "none";
+      logoutBtn.style.display = "inline-block";
+      loginStatus.innerHTML = "You are logged in!";
+    } else {
+      loginBtn.style.display = "inline-block";
+      logoutBtn.style.display = "none";
+      loginStatus.innerHTML =
+        "You are not logged in! Please log in to continue.";
+    }
+  }
+
+  // And once everything is located
+  handleAuthentication();
+});  
+
